958d2688d1c1b8b283ba99e229e4d2e32a82f898
arismendy56 <arismendylarancuent@gmail.com>
2020-01-15 21:23:07 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, RootRenderer } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+  `
+})
+export class AppComponent {
+  pageTitle: string = 'Acme Product Management';
+}
